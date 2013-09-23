843b68f0af116ce173b82e901b91097ed0d7b0ad
kasthack <kasthack@epicm.org>
2013-09-23 17:18:23 +0400

    progressbar
    Signed-off-by: kasthack <kasthack@epicm.org>

--- staticfiles/js/page_progress.js
+++ staticfiles/js/page_progress.js
@@ -1,33 +1,125 @@
 function fetchData(){
 	$.ajax ({
-        type: "GET",
+        type: 'GET',
         url: 'progress',
         dataType: 'json',
         async: false,
-        data: { "timestamp": window.timestamp, "pageid" : window.pageid },
+        data: { 'timestamp': window.Timestamp, 'pageid' : window.PageID },
         success: function (data) {
-         window.data = data;
+         //process
+		 window.data = data;
         }
     });
+}
+//removeClassIfExists
+function rCIF(element, classname) {
+	if (element.hasClass(classname))
+		return element.removeClass(classname)
+}
+//addClassIfNotExists
+function aCINF(element, classname) {
+	if (!element.hasClass(classname))
+		return element.addClass(classname)
+}
+//mpc - main progress container, mpb - main progress bar
+//progressBarShowZero
+function pBSZ(mpc, mpb){
+	aCINF(mpc,s_pcss);
+	rCIF(mpb, s_pbss);
+	rCIF(mpb, s_pbds);
+	aCINF(mpb,s_pbis);
+	mpb.width(mpc.width());
+}
+//mpc - main progress container, mpb - main progress bar
+//progressBarShowWIN
+function pBSW(mpc, mpb){
+	rCIF(mpc,s_pcss);
+	rCIF(mpb, s_pbis);
+	rCIF(mpb, s_pbds);
+	aCINF(mpb,s_pbss);
+	mpb.width(mpc.width());
+}
+//mpc - main progress container, mpb - main progress bar
+//progressBarShowError
+function pBSE(mpc, mpb){
+	rCIF(mpc,s_pcss);
+	rCIF(mpb, s_pbis);
+	rCIF(mpb,s_pbss);
+	aCINF(mpb, s_pbds);
+	mpb.width(mpc.width());
+}
+function progressBarUpdate(){
+	if (window.e_pr) {
+		dbgOut('Progressbar update began');
+		if (window.data !== undefined){
+			var pageinfo = window.data["page"];
+			var ready = parseInt(pageinfo['ready']);
+			var total = parseInt(pageinfo['total']);
+			var status = parseInt(pageinfo['status']);
+			dbgOut('Progress render start');
+			
+			var mpc = $('#mpc');
+			var mpb = $('#mpb');
+			
+			switch(status) {
+				case 0:
+					pBSZ(mpc,mpb);
+					break;
+				case 1:
+					if (total>0){
+						mpb.width(mpc.width()*ready/total);
+						if (ready == total){
+							e_pr = false;
+							pBSW(mpc, mpb);
+						}
+						else {
+							rCIF(mpc, s_pcss);
+							rCIF(mpb, s_pbss);
+							rCIF(mpb, s_pbds);
+							aCINF(mpb,s_pbis);
+						}
+					}
+					else {
+						pBSZ(mpc,mpb);
+					}
+					dbgOut('Progress bar updated');
+					break;
+					break;
+				case 2:
+					e_pr = false;
+					pBSW(mpc, mpb);
+					break;
+				case 3:
+				case 4:
+					e_pr = false;
+					pBSE(mpc, mpb);
+					break;
+
+			}
+			dbgOut('Progress render complete');
+		}
+	}	
 }
 function startProgress() {
 	setTimeout(progress, 0);
 }
 function dbgOut(message) {
-	console.log(message);
+	if (e_vb) {
+		console.log(message);
+	}
 }
 //update loop
 function progress() {
-	if (progress_enabled) {
+	if (window.e_pf) {
 		dbgOut ('Progress started');
-		if (!fetch_running){
+		if (!window.FetchRunning){
 			dbgOut ('Condition success');
-			fetch_running = true;
+			window.FetchRunning = true;
 			if(window.status < 2){
 				fetchData();
 				try
 				{
-					if (window.data["result"]==="ok")
+					if (window.data['result']==='ok')
 						dbgOut('Data ok');
 					else
 						dbgOut('Data error');
@@ -36,9 +128,9 @@ function progress() {
 					dbgOut('JSON error');
 				}
 			}
-			fetch_running = false;
+			window.FetchRunning = false;
 			dbgOut ('Progress exited');
-			setTimeout(progress, window.check_interval);
 		}
 	}
+	setTimeout(progress, window.CheckInterval);
 }
