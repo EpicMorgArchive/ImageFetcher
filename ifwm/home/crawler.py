f10907abf1495b4175a5f79fbbe0292b532cc4db
kasthack <kasthack@epicm.org>
2013-09-29 17:28:03 +0400

    Revert "bad"
    This reverts commit ecb23ac9599019bae951b4dc268df4fd8024f073.

---
+++ ifwm/home/crawler.py
@@ -0,0 +1,308 @@
+# coding=utf-8
+from random import random
+from bs4 import BeautifulSoup
+from django.db.models import Q
+from ifwm.home.helpClasses import getTime, getMD5Str, dbgOut
+from ifwm.home.models import Pages, Urls, Images
+from time import sleep
+from ifwm import settings
+import os.path
+import re
+import traceback
+import urllib2
+import urlparse
+
+#works
+def StartCrawler():
+    dbgOut('CrawlerRequested')
+    glb = globals()
+    if not ('CrawlerStarted' in vars() or 'CrawlerStarted' in glb):
+        glb['CrawlerStarted'] = True
+        dbgOut('Crawler started')
+        from threading import Thread
+
+        glb['thread'] = Thread(target=_MainLoop)
+        glb['thread'].start()
+
+
+#works
+def urlopenUA(url):
+    request = urllib2.Request(url=url, headers={'User-Agent': "Magic Browser"})
+    return urllib2.urlopen(request, timeout=3000)
+
+
+#works
+def _parseIntOrZero(string):
+    try:
+        return int(string)
+    except:
+        return 0
+
+#works
+def _getPages():
+    return list(
+        Pages.objects.filter(
+            Q(url__status=0) | Q(url__status=3)
+        )
+    )
+
+#works
+def _copyStream(read_stream, write_stream, count):
+    downloaded = 0
+    buffersize = 16384
+    try:
+        while downloaded < count:
+            writebuf = read_stream.read(buffersize)
+            if not writebuf:
+                break
+            downloaded += len(writebuf)
+            write_stream.write(writebuf)
+    finally:
+        read_stream.close()
+        write_stream.close()
+
+#works
+def _findImageUrlsOnPage(page_str):
+    parser = BeautifulSoup(page_str)
+    imgurls = []
+    for img in parser.find_all('img'):
+        imgurls.append(
+            img.get('src')
+        )
+    del parser
+    return imgurls
+
+
+#works
+def _filterDomainImages(imgurls, pageurl):
+    finurls = {}
+    pagehost = '.'.join(
+        urlparse.urlparse(pageurl).netloc.split('.')[-2:]
+    )
+    for img in imgurls:
+        try:
+            o = urlparse.urlparse(img)
+        except:
+            o = ''
+        nl = o.netloc
+        if nl == '':
+            curl = urlparse.urljoin(pageurl, o.path)
+        elif nl == pagehost or nl.endswith('.' + pagehost):
+            curl = img
+        else:
+            curl = None
+        del o
+        if not curl:
+            continue
+        imghash = getMD5Str(curl)
+        #distinct
+        if imghash in finurls:
+            continue
+        finurls[imghash] = curl
+        del curl
+    del imgurls
+    return finurls
+
+
+def _fetchImg(img, save_dir):
+    try:
+        dbgOut(
+            'Downloading image "%s"  to %d' % img.url.url, img.url.id
+        )
+        url = img.url
+        url.status = 1
+        url.save()
+        image_response = urlopenUA(url.url)
+        #region check size
+        try:
+            size_str = image_response.headers['content-length']
+        except KeyError:
+            size_str = '0'
+        size = _parseIntOrZero(size_str)
+        url.date = getTime()
+        content_type = image_response.headers['content-type']
+        if (
+                (size > settings.MAX_FETCH_IMAGE_SIZE) or (
+                    not content_type
+                ) or (
+                    not content_type.startswith('image/')
+                )
+        ):
+            dbgOut('Banned image "%s"' % img.url.url)
+            url.status = 4
+            url.save()
+            return
+            #extension
+        if img.ext:
+            ext = img.ext
+        else:
+            ext = re.search(
+                'image/(\w+)',
+                content_type
+            ).group(0)
+        save_filename = os.path.join(save_dir, str(url.id) + '.' + ext)
+        write_stream = open(save_filename, 'wb')
+        if size == 0:
+            max_dwnld = settings.MAX_FETCH_IMAGE_SIZE
+        else:
+            max_dwnld = size
+        try:
+            _copyStream(image_response, write_stream, max_dwnld)
+            dbgOut('Successfully downloaded image %s' % img.url.url)
+        except Exception, e:
+            os.remove(save_filename)
+            raise e
+
+        url.status = 2
+    except:
+        dbgOut(
+            'Error occurred while downloading %s' % img.url.url
+        )
+        url.status = 3
+    finally:
+        url.save()
+
+#find queued images and add to DB
+#return their urls
+
+def _addImagesFromPage(page):
+    try:
+        pageurl = page.url.url
+        dbgOut('Downloading page "%s"' % pageurl)
+        result = urlopenUA(pageurl)
+        returls = {}
+        #check size
+        #ban big pages and not web pages
+        str_size = result.headers['content-length']
+        size = _parseIntOrZero(str_size)
+        page.url.date = getTime()
+        if size > settings.MAX_FETCH_PAGE_SIZE or (
+            not result.headers['content-type'].startswith('text/')
+        ):
+            dbgOut('Banned page "%s"' % page.url.url)
+            page.url.status = 4
+            page.url.save()
+            return returls
+            #get urls
+        parsetext = result.read()
+        imgurls = _findImageUrlsOnPage(parsetext)
+        del result
+        if not imgurls:
+            return returls
+            #find urls to this domain + subdomains
+        goodurls = _filterDomainImages(
+            imgurls,
+            pageurl
+        )
+        del imgurls
+        if not goodurls:
+            return returls
+        dbgOut('Got %d images from page "%s"' % (len(goodurls), page.url.url))
+            #remove all !error queued
+        #remove existing urls those don't need to be fetched
+        #update
+        hashes = goodurls.keys()
+        images = Images.objects.filter(
+            url__urlhash__in=hashes
+        )
+        images.prefetch_related('url')
+        existing_urls_list = list(images)
+        del images
+        del hashes
+        update_status = []
+        for img in existing_urls_list:
+            if img.url.status == 3:
+                dbgOut(
+                    'Url with hash %s added to update list' % img.url.url
+                )
+                update_status.append(img.url.urlhash)
+                returls[img.url.urlhash] = img
+            del goodurls[img.url.urlhash]
+        del existing_urls_list
+        if update_status:
+            Urls.objects.filter(
+                urlhash__in=update_status
+            ).update(
+                urlhash=0
+            )
+        del update_status
+        #insert new items to db
+        #TODO: move all to one transaction
+
+        sql_urls = []
+        sql_images = []
+        for imghash, img in goodurls.iteritems():
+            if not imghash in returls:
+                dbgOut(
+                    'Url with hash %s added to download list' % imghash
+                )
+                tmp_img_url = Urls()
+                tmp_img_url.urlhash = imghash
+                tmp_img_url.url = img
+                tmp_img_url.status = 0
+                sql_urls.append(tmp_img_url)
+        saved_sql_urls = Urls.objects.bulk_create(sql_urls)
+        for imgurl in saved_sql_urls:
+            tmp_img = Images()
+            tmp_img.ext = os.splitext(imgurl.url)[1]
+            tmp_img.page = page
+            tmp_img.url = tmp_img_url
+            sql_images.append(tmp_img)
+            returls[imgurl.urlhash] = tmp_img
+            del goodurls[imgurl.urlhash]
+        Images.objects.bulk_create(sql_images)
+        del sql_urls
+        del sql_images
+        dbgOut(
+            'Parsing %s complete' % page.url.url
+        )
+        return returls
+    except:
+        page.url.status = 3
+        page.url.save()
+        page.save()
+
+
+def _MainLoop():
+    dbgOut('Crawler works')
+    while True:
+        sleep(5)
+        try:
+            #if True:
+            #    dbgOut('ContinueWorks!')
+            #    continue
+            dbgOut('Connecting to DB')
+            pages = _getPages()
+            dbgOut('Got %d pages from DB' % len(pages))
+            if not pages:
+                continue
+                #images to download
+            images_hashes = {}
+            for page in pages:
+                tmp_img_hsh = _addImagesFromPage(page)
+                if not tmp_img_hsh:
+                    continue
+                for imghash in tmp_img_hsh.keys():
+                    images_hashes[imghash] = tmp_img_hsh[imghash]
+                del tmp_img_hsh
+                dbgOut('Got %d images' % len(images_hashes))
+                if not images_hashes:
+                    continue
+                for imghash in sorted(
+                        images_hashes.keys(),
+                        key=lambda x: random.random()
+                ):
+                    img = images_hashes[imghash]
+                    _fetchImg(img, settings.MEDIA_ROOT, settings.MAX_FETCH_IMAGE_SIZE)
+                    del img
+                    del images_hashes[imghash]
+        except Exception, e:
+            trace = str(traceback.extract_stack()[-1][1])
+            dbgOut(
+                (
+                    'Exception occurred: %r %s' % (e, trace)
+                ),
+                3
+            )
+            #return
+            continue
---
+++ ifwm/home/views.py
@@ -0,0 +1,217 @@
+#! /usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.conf.urls import url
+from django.core import serializers
+from django.core.validators import URLValidator
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils import simplejson
+from django.views.generic import TemplateView, View
+from django.views.decorators.csrf import csrf_exempt
+from ifwm.home.crawler import StartCrawler
+from ifwm.home.helpClasses import *
+from ifwm.home.models import Pages, Images, Urls
+from django.core.context_processors import request
+
+Validator = URLValidator()
+
+
+def showErrorPage(request, errorname, errortext, status=200):
+    errorpage = ErrorPageView()
+    errorpage.request = request
+    error = Error(errorname, errortext)
+    return errorpage.showError(error, status)
+
+
+def showImagesPage(request, url):
+    ipage = ImagesPageView()
+    ipage.request = request
+    return ipage.showPage(url)
+
+
+class ErrorPageView(TemplateView):
+    template_name = 'error.html'
+
+    def showError(self, error, status=200):
+    	StartCrawler()
+        return render(
+            self.request,
+            self.template_name,
+            {
+                'error': error
+            },
+            status=status
+        )
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+    	StartCrawler()
+        return self.render_to_response({})
+
+
+class AddUrlView(TemplateView):
+    def _insertUrl(self, url, hasher):
+        urlinfo = Urls()
+        urlinfo.urlhash = hasher
+        urlinfo.url = url
+        urlinfo.save()
+        page = Pages()
+        page.url = urlinfo
+        page.save()
+        return urlinfo
+
+    def post(self, request, *args, **kwargs):
+    	StartCrawler()
+        url = request.POST.get('url', '')
+        if not url:
+            return showErrorPage(
+                request,
+                'Bad url',
+                'Empty url',
+                400
+            )
+        try:
+            Validator(url)
+        except:
+            url = 'http://%s' % url
+            try:
+                Validator(url)
+            except:
+                return showErrorPage(
+                    request,
+                    'Bad url',
+                    'Invalid url',
+                    400
+                )
+        hasher = getMD5Str(url)
+        dburls = Urls.objects.filter(urlhash=hasher)[:1]
+        if dburls:
+            urlinfo = dburls[0]
+            #link to image
+            if not Pages.objects.filter(url=urlinfo.id)[:1]:
+                return showErrorPage(
+                    request,
+                    'Bad url',
+                    'You are trying to add image',
+                    400
+                )
+            if urlinfo.status == 4:
+                return showErrorPage(
+                    request,
+                    'Bad url',
+                    'This url was banned',
+                    403
+                )
+            elif urlinfo.status == 2 | urlinfo.status == 3:
+                urlinfo.status = 0
+                urlinfo.save()
+        else:
+            try:
+                urlinfo = self._insertUrl(url, hasher)
+            except:
+                return showErrorPage(
+                    request,
+                    'Bad url',
+                    'Invalid url',
+                    400
+                )
+        return redirect('/page%d' % urlinfo.id)
+
+
+class ImagesPageView(TemplateView):
+    template_name = 'image_list.html'
+
+    def _showPage(self, url, page, *args, **kwargs):
+        images = page.images_set.all()
+        images.prefetch_related('url')
+        context = {
+            'page': page,
+            'images': images,
+            'url': url,
+            'timestamp': getTime(),
+            'args' : args,
+            'kwargs' : kwargs
+        }
+        return self.render_to_response(context)
+
+    def showPage(self, url):
+        return self.showPageF(url, Pages.objects.get(pk=url.id))
+
+    def showPageF(self, url, page):
+        return self._showPage(url, page, None, None)
+
+    def get(self, request, *args, **kwargs):
+    	StartCrawler()
+        pageid = 0
+        sid = kwargs.get('id')
+        try:
+            pageid = int(sid)
+            page = Pages.objects.get(pk=pageid)
+        except Exception, e:
+            return showErrorPage(
+                request,
+                '404',
+                'Nothing found',
+                404
+            )
+        return self._showPage(
+            page.url,
+            page,
+            args,
+            kwargs
+        )
+
+
+class PageProgress(View):
+    def _badReq(self):
+        return HttpResponse(
+            '{"result": "error" } ',
+            mimetype='application/json',
+            status=400
+        )
+
+    def resp(self, params):
+        try:
+            #parse
+            sctimestamp = params.get('timestamp')
+            spageid = params.get('pageid')
+            ctimestamp = int(sctimestamp)
+            pageid = int(spageid)
+            sctimestamp = None
+            spageid = None
+            #date check
+            timestamp = getTime()
+            if ctimestamp > timestamp:
+                return self._badReq()
+            #db fetch
+            page = Pages.objects.get(pk=pageid)
+            images = page.images_set.exclude(
+                url__status__lt=2
+            ).filter(
+                url__date__gte=ctimestamp
+            )
+            images.prefetch_related('url')
+            #jsonize
+            pi = ProgressInfo(
+                page,
+                timestamp,
+                images
+            )
+            return HttpResponse(
+                pi.getJson(),
+                mimetype='application/json'
+            )
+        except:
+            return self._badReq()
+
+    def post(self, request, *args, **kwargs):
+    	StartCrawler()
+        return  self.resp(request.POST)
+
+    def get(self, request, *args, **kwargs):
+    	StartCrawler()
+        return  self.resp(request.GET)
